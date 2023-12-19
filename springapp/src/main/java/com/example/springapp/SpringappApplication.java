19c8fa476ea95a6823d4c0121f1e78e4941292de
neojarvis <techadmin@iamneo.ai>
2023-12-19 12:15:59 +0000

    Manual-save-commit-12/19/2023--5:45:48-PM

--- springapp/src/main/java/com/example/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -22,3 +22,54 @@ public class SpringappApplication {
 	}
 
 }
+
+package com.example.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringappApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(SpringappApplication.class, args);
+	   
+		//Write Your code Here 
+
+
+    int sum=obj.add();
+    int diff=obj.sub();
+    System.out.println(sum);
+    System.out.println(diff);
+		
+	}
+
+}
+
+
+package com.example.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringappApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(SpringappApplication.class, args);
+	   
+		//Write Your code Here 
+
+
+    int sum=obj.add();
+    int diff=obj.sub();
+    System.out.println(sum);
+    System.out.println(diff);
+		
+	}
+
+}
